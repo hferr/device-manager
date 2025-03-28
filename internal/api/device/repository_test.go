90bc8cb0e757b2317f43acc9b506f74e8d3c9c32
hferr <hferrsj@gmail.com>
2025-03-28 18:42:00 -0300

    adds 'FindByState' and 'FindByBrand' repo tests

--- internal/api/device/repository_test.go
+++ internal/api/device/repository_test.go
@@ -107,3 +107,99 @@ func TestDeleteDevice(t *testing.T) {
 		t.Fatal("expected error, got none")
 	}
 }
+
+func TestFindByState(t *testing.T) {
+	cleanup, db := test.SetupTestDBContainer(t)
+	defer cleanup()
+
+	repo := device.NewRepository(db)
+
+	devicesInUse := []*device.Device{
+		device.NewDevice("test", "test", "in_use"),
+		device.NewDevice("test", "test", "in_use"),
+		device.NewDevice("test", "test", "in_use"),
+	}
+
+	devicesAvailable := []*device.Device{
+		device.NewDevice("test", "test", "available"),
+		device.NewDevice("test", "test", "available"),
+	}
+
+	allDevices := append(devicesInUse, devicesAvailable...)
+
+	for _, d := range allDevices {
+		if err := repo.InsertDevice(d); err != nil {
+			t.Fatalf("expected no error, got: %v", err)
+		}
+	}
+
+	// fetch by state 'in_use'
+
+	devicesInUseFromDB, err := repo.FindByState("in_use")
+	if err != nil {
+		t.Fatalf("expected no error, got: %v", err)
+	}
+
+	if len(devicesInUse) != len(devicesInUseFromDB) {
+		t.Fatalf("expected %d devices 'in_use', got: %d", len(devicesInUse), len(devicesInUseFromDB))
+	}
+
+	// fetch by state 'available'
+
+	devicesAvailableFromDB, err := repo.FindByState("available")
+	if err != nil {
+		t.Fatalf("expected no error, got: %v", err)
+	}
+
+	if len(devicesAvailable) != len(devicesAvailableFromDB) {
+		t.Fatalf("expected %d devices 'in_use', got: %d", len(devicesAvailable), len(devicesAvailableFromDB))
+	}
+}
+
+func TestFindByBrand(t *testing.T) {
+	cleanup, db := test.SetupTestDBContainer(t)
+	defer cleanup()
+
+	repo := device.NewRepository(db)
+
+	devicesWithBrand1 := []*device.Device{
+		device.NewDevice("test", "cool_brand", "in_use"),
+		device.NewDevice("test", "cool_brand", "in_use"),
+		device.NewDevice("test", "cool_brand", "in_use"),
+	}
+
+	devicesWithBrand2 := []*device.Device{
+		device.NewDevice("test", "nice_brand", "available"),
+		device.NewDevice("test", "nice_brand", "available"),
+	}
+
+	allDevices := append(devicesWithBrand1, devicesWithBrand2...)
+
+	for _, d := range allDevices {
+		if err := repo.InsertDevice(d); err != nil {
+			t.Fatalf("expected no error, got: %v", err)
+		}
+	}
+
+	// fetch by brand 'cool_brand'
+
+	devicesWithBrand1FromDB, err := repo.FindByBrand("cool_brand")
+	if err != nil {
+		t.Fatalf("expected no error, got: %v", err)
+	}
+
+	if len(devicesWithBrand1) != len(devicesWithBrand1FromDB) {
+		t.Fatalf("expected %d devices, got: %d", len(devicesWithBrand1), len(devicesWithBrand1FromDB))
+	}
+
+	// fetch by brand 'nice_brand'
+
+	devicesWithBrand2FromDB, err := repo.FindByBrand("nice_brand")
+	if err != nil {
+		t.Fatalf("expected no error, got: %v", err)
+	}
+
+	if len(devicesWithBrand2) != len(devicesWithBrand2FromDB) {
+		t.Fatalf("expected %d devices, got: %d", len(devicesWithBrand2), len(devicesWithBrand2FromDB))
+	}
+}
