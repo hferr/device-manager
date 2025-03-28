053cfd701ee5bb24160aad5a068f87e7f59fc60c
hferr <hferrsj@gmail.com>
2025-03-28 15:17:32 -0300

    adds 'device' new, dto and create request structs and funcs

--- internal/api/device/model.go
+++ internal/api/device/model.go
@@ -13,3 +13,37 @@ type Device struct {
 	State     string
 	CreatedAt time.Time
 }
+
+type DTO struct {
+	ID        uuid.UUID `json:"id"`
+	Name      string    `json:"name"`
+	Brand     string    `json:"brand"`
+	State     string    `json:"state"`
+	CreatedAt string    `json:"created_at"`
+}
+
+type CreateDeviceRequest struct {
+	Name  string `json:"name" validate:"required,max=255"`
+	Brand string `json:"brand" validate:"required,max=255"`
+	State string `json:"state" validate:"required,oneof=available in_use inactive"`
+}
+
+func NewDevice(name, brand, state string) *Device {
+	return &Device{
+		ID:        uuid.New(),
+		Name:      name,
+		Brand:     brand,
+		State:     state,
+		CreatedAt: time.Now(),
+	}
+}
+
+func (d *Device) ToDTO() *DTO {
+	return &DTO{
+		ID:        d.ID,
+		Name:      d.Name,
+		Brand:     d.Brand,
+		State:     d.State,
+		CreatedAt: d.CreatedAt.Format(time.DateTime),
+	}
+}
