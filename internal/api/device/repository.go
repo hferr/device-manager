9b076b3c687f4da4a0b389fd0d05604eda74ebc9
hferr <hferrsj@gmail.com>
2025-03-28 15:18:26 -0300

    adds 'InsertDevice' repo func

--- internal/api/device/repository.go
+++ internal/api/device/repository.go
@@ -2,7 +2,9 @@ package device
 
 import "gorm.io/gorm"
 
-type DeviceRepository interface{}
+type DeviceRepository interface {
+	InsertDevice(device *Device) error
+}
 
 type deviceRepository struct {
 	db *gorm.DB
@@ -12,4 +14,12 @@ func NewRepository(db *gorm.DB) DeviceRepository {
 	return &deviceRepository{
 		db: db,
 	}
+}
+
+func (d *deviceRepository) InsertDevice(device *Device) error {
+	if err := d.db.Create(device).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
